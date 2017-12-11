8f8b086e5390c101bbd432f4151337afcd9be476
Thomas Fahrmeyer <thomas.fahrmeyer@meditec.zeiss.com>
2017-12-11 09:46:38 +0100

    - renaming of the module p2-repo-generator - added comments

--- repo-lifecycle-extension/src/main/java/de/zeiss/maven/extension/TychoLifecycleController.java
+++ repo-lifecycle-extension/src/main/java/de/zeiss/maven/extension/TychoLifecycleController.java
@@ -8,6 +8,7 @@ import org.apache.maven.eventspy.AbstractEventSpy;
 import org.apache.maven.eventspy.EventSpy;
 import org.apache.maven.execution.ExecutionEvent;
 import org.apache.maven.execution.MavenSession;
+import org.apache.maven.project.MavenProject;
 import org.codehaus.plexus.PlexusContainer;
 import org.codehaus.plexus.classworlds.realm.ClassRealm;
 import org.codehaus.plexus.component.annotations.Component;
@@ -16,15 +17,28 @@ import org.codehaus.plexus.component.repository.exception.ComponentLookupExcepti
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+/**
+ * This class controlls a {@link AbstractMavenLifecycleParticipant} implementation provided by Tycho itself.
+ * Controlling in this context means that the lifecycle method {@link AbstractMavenLifecycleParticipant#afterProjectsRead(MavenSession)}
+ * is delayed until the maven plugin that builds the local p2-repository is finished. If the plugin is finished the mentioned lifecycle method is
+ * called. Otherwise the tycho prozess for validating the p2 repository used to build the target platform and the product will fail, because the repository
+ * does not yet exists.
+ * </p>
+ * The functionality was tested with tycho code version 1.0.0.
+ *
+ */
 @Component(role = EventSpy.class, hint = "explore")
 public class TychoLifecycleController extends AbstractEventSpy {
 
     private static final String PACKAGE_PLUGIN_ARTIFACT = "maven-osgi-package-plugin";
     private static final String PACKAGE_PLUGIN_GOAL = "package-p2-repo";
+
+    // tycho specific properties. Captured from Tycho Core Version 1.0.0
     private static final String TYCHO_DISABLE_PROPERTY = "tycho.mode";
     private static final String TYCHO_OFF_VALUE = "maven";
     private static final String TYCHO_ON_VALUE = "";
     private static final String TYCHO_CLASSLOADER_ID_PART = "tycho-maven-plugin";
+
     private final Logger logger = LoggerFactory.getLogger(getClass());
 
     @Requirement
@@ -49,7 +63,7 @@ public class TychoLifecycleController extends AbstractEventSpy {
     private void onEvent(ExecutionEvent event) throws Exception {
         switch (event.getType()) {
             case ProjectDiscoveryStarted:
-                event.getSession().getUserProperties().put(TYCHO_DISABLE_PROPERTY, TYCHO_OFF_VALUE);
+                disableTychoExecutionInAfterProjectsReadLifecycle(event.getSession());
 
                 break;
 
@@ -65,15 +79,16 @@ public class TychoLifecycleController extends AbstractEventSpy {
         }
     }
 
+
     /**
      * Starts the tycho process by retrieving the tycho {@link AbstractMavenLifecycleParticipant} implementation and executing
      * the method {@link AbstractMavenLifecycleParticipant#afterProjectsRead(MavenSession)}.
-     * That prepares the following tycho process.
+     * That prepares the following tycho process for building the target platform and packaging the product.
      *
      * @param event
      */
     private void startTychoProcess(ExecutionEvent event) {
-        Optional<ClassRealm> tychoRealm = findTychoClassRealm(event);
+        Optional<ClassRealm> tychoRealm = findTychoClassRealm(event.getSession().getCurrentProject());
         if (!tychoRealm.isPresent()) {
             logger.error("Could not find the tycho classRealm and can not work properly. Give up.");
             return;
@@ -84,8 +99,7 @@ public class TychoLifecycleController extends AbstractEventSpy {
 
         try {
             AbstractMavenLifecycleParticipant tychoMavenLifecycleListener = container.lookup(AbstractMavenLifecycleParticipant.class, "TychoMavenLifecycleListener");
-            // enable the tycho stuff
-            event.getSession().getUserProperties().put(TYCHO_DISABLE_PROPERTY, TYCHO_ON_VALUE);
+            enableTychoExecutionInAfterProjectsReadLifecycle(event.getSession());
 
             tychoMavenLifecycleListener.afterProjectsRead(event.getSession());
 
@@ -100,8 +114,24 @@ public class TychoLifecycleController extends AbstractEventSpy {
         }
     }
 
-    private Optional<ClassRealm> findTychoClassRealm(ExecutionEvent event) {
-        Optional<ClassRealm> tychoRealm = event.getSession().getCurrentProject().getClassRealm().getWorld().getRealms().stream().filter(realm -> realm.getId()
+    private void enableTychoExecutionInAfterProjectsReadLifecycle(MavenSession session) {
+        // enable the tycho stuff
+        session.getUserProperties().put(TYCHO_DISABLE_PROPERTY, TYCHO_ON_VALUE);
+    }
+
+    private void disableTychoExecutionInAfterProjectsReadLifecycle(MavenSession session) {
+        session.getUserProperties().put(TYCHO_DISABLE_PROPERTY, TYCHO_OFF_VALUE);
+    }
+
+    /**
+     * Scans all available {@link ClassRealm}'s managed by the maven container and find the one with
+     * an ID containing {@link #TYCHO_CLASSLOADER_ID_PART}
+     *
+     * @return an Optional with the found {@code ClassRealm} or {@code null}.
+     * @param currentProject
+     */
+    private Optional<ClassRealm> findTychoClassRealm(MavenProject currentProject) {
+        Optional<ClassRealm> tychoRealm = currentProject.getClassRealm().getWorld().getRealms().stream().filter(realm -> realm.getId()
                 .contains(TYCHO_CLASSLOADER_ID_PART)).findFirst();
 
         return tychoRealm;
